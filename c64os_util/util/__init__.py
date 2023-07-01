debab312e4b0d23d8ec2c3bb88c6e9d6fda19b51
voidstar <voidstar@anachronauts.club>
2023-07-01 15:14:14 -0400

    Remove cbmcodecs2 dependency

--- c64os_util/util/__init__.py
+++ c64os_util/util/__init__.py
@@ -1,2 +1,12 @@
-from .codec import LC_CODEC, UC_CODEC
+import codecs
+import typing
+
+from .codec import CODEC_INFOS, LC_CODEC, UC_CODEC
 from .functions import copy_buffer
+
+
+def petscii_search_fn(encoding: str) -> typing.Optional[codecs.CodecInfo]:
+    return CODEC_INFOS.get(encoding, None)
+
+
+codecs.register(petscii_search_fn)
