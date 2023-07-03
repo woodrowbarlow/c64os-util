e3628a0315f0df98802f7cf073ad97ebc5ef467d
voidstar <voidstar@anachronauts.club>
2023-07-03 18:38:43 -0400

    [docs] Use RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,10 @@ author = "voidstar"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_rtd_theme",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -27,6 +30,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = 'sphinx_rtd_theme'
 html_static_path = []
 html_templates_path = []
